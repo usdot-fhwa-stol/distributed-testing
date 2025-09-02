57db15fee12f06701753704ee59a85d7b0831bb2
Andrew Loughran <andrew.j.loughran@gmail.com>
2025-09-02 14:24:11 -0400

    updated engage script

--- scripts/run_scripts/engage-carma-platform.sh
+++ scripts/run_scripts/engage-carma-platform.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-carma exec 'source /opt/carma/install_ros2/setup.bash && ros2 service call /guidance/set_guidance_active carma_planning_msgs/srv/SetGuidanceActive "{guidance_active: true}"'
+carma exec -n carma_carma_net 'source /opt/carma/install_ros2/setup.bash && ros2 service call /guidance/set_guidance_active carma_planning_msgs/srv/SetGuidanceActive "{guidance_active: true}"'
