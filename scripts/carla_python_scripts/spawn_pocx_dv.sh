c38e0f7d6921eff704ff4cad4763203e84c14f4f
Andrew Loughran <andrew.j.loughran@gmail.com>
2025-09-10 16:04:35 -0400

    pushed final scripts

---
+++ scripts/carla_python_scripts/spawn_pocx_dv.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+python3 voices-poc/scripts/carla_python_scripts/manual_control_keyboard_no_destroy.py --x 26.360575 --y -417.402069 --z 38.5 --yaw -175.119400 --rolename DV --filter vehicle.*
